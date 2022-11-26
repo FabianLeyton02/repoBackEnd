c7c37e81d87f5d94f207657cde8ca634615d6e97
Fabian Leyton <fabianleyton02@gmail.com>
2022-11-26 09:59:53 -0300

    creación del repositorio

---
+++ ProductManager.js
@@ -0,0 +1,120 @@
+class Product {
+  constructor(id, title, description, price, thumbnail, code, stock) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.price = Number(price);
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = Number(stock);
+  }
+}
+
+class ProductManager {
+  constructor() {
+    this.products = [];
+  }
+
+  addProduct(title, description, price, thumbnail, code, stock) {
+    let validado = true;
+
+    if (!title) {
+      validado = false;
+      console.error("ERROR: title is undefined");
+    }
+
+    if (!description) {
+      validado = false;
+      console.error("ERROR: description is undefined");
+    }
+
+    if (!price) {
+      validado = false;
+      console.error("ERROR: price is undefined");
+    } else {
+      if (typeof price != "number") {
+        console.error("ERROR: price is not a number");
+        validado = false;
+      }
+    }
+
+    if (!thumbnail) {
+      validado = false;
+      console.error("ERROR: thumbnail is undefined");
+    }
+
+    if (!code) {
+      validado = false;
+      console.error("ERROR: code is undefined");
+    }
+
+    if (!stock) {
+      validado = false;
+      console.error("ERROR: stock is undefined");
+    } else {
+      if (typeof stock != "number") {
+        validado = false;
+        console.error("ERROR: stock is not a number");
+      }
+    }
+    let productFound = false;
+    if (validado) {
+      this.products.forEach((element) => {
+        if (element.code === code) {
+          productFound = true;
+        }
+      });
+
+      if (productFound) {
+        console.error("ERROR: product already exists");
+      } else {
+        let id = this.products.length + 1;
+        this.products.push(
+          new Product(id, title, description, price, thumbnail, code, stock)
+        );
+      }
+    }
+  }
+
+  getProducts() {
+    console.log(this.products);
+  }
+  getProductById(id) {
+    let product = undefined;
+    this.products.forEach((element) => {
+      if (element.id === id) {
+        product = element;
+      }
+    });
+    if (product === undefined) {
+      console.error("ERROR: not found");
+    } else {
+      console.log(product);
+    }
+  }
+}
+
+let productManager = new ProductManager();
+
+productManager.getProducts();
+productManager.addProduct(
+  "producto prueba",
+  "Este es un producto de prueba",
+  200,
+  "Sin imagen",
+  "abc123",
+  25
+);
+
+productManager.getProducts();
+productManager.addProduct(
+  "producto prueba",
+  "Este es un producto de prueba",
+  200,
+  "Sin imagen",
+  "abc123",
+  25
+);
+
+productManager.getProductById(1);
+productManager.getProductById(2);
